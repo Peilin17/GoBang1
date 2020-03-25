5c7b7316602962d5b27c608a8dcd71c6b9fed03f
Xiaolin Zhou <42752035+xiaolin98@users.noreply.github.com>
2020-03-25 17:19:14 -0400

    change background image function

--- app/src/main/java/com/example/gobang/MainActivity.kt
+++ app/src/main/java/com/example/gobang/MainActivity.kt
@@ -4,13 +4,20 @@ package com.example.gobang
 test push Xiaolin
  */
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.View
+import android.widget.Toast
 import com.example.gobang.com.example.gobang.AIChessboardView
 import kotlinx.android.synthetic.main.activity_main.*
+import android.Manifest
+import android.os.Build
+import android.os.Build.*
 
 const val MAX_COUNT_IN_LINE = 5
 const val MAX_LINE = 15
@@ -77,6 +84,71 @@ class MainActivity : AppCompatActivity() {
             } else {
                 findViewById<AIChessboardView>(R.id.boardView2).regretPlay()
             }
+        }
+
+        // change background button
+        changebackground_button.setOnClickListener {
+            //check runtime permission
+            if (VERSION.SDK_INT >= VERSION_CODES.M) {
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_DENIED
+                ) {
+                    //permission denied
+                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE);
+                    //show popup to request runtime permission
+                    requestPermissions(permissions, PERMISSION_CODE);
+                } else {
+                    //permission already granted
+                    pickImageFromGallery();
+                }
+            } else {
+                //system OS is < Marshmallow
+                pickImageFromGallery();
+            }
+        }
+    }
+
+    companion object {
+        //image pick code
+        private val IMAGE_PICK_CODE = 1000;
+
+        //Permission code
+        private val PERMISSION_CODE = 1001;
+    }
+
+
+    private fun pickImageFromGallery() {
+        //Intent to pick image
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+    //handle requested permission result
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSION_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED
+                ) {
+                    //permission from popup granted
+                    pickImageFromGallery()
+                } else {
+                    //permission from popup denied
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    //handle result of picked image
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+            background_imageview.setImageURI(data?.data)
         }
     }
 
