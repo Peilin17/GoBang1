f7be90b7d9c4b06ce194a509571fdade99883701
Xiaolin Zhou <42752035+xiaolin98@users.noreply.github.com>
2020-04-23 22:08:23 -0400

    can change background by url
    add background_spinner

--- app/src/main/java/com/example/gobang/MainActivity.kt
+++ app/src/main/java/com/example/gobang/MainActivity.kt
@@ -29,11 +29,11 @@ import android.os.Message
 import android.text.TextUtils
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModelProvider
 import com.example.gobang.com.example.gobang.AIChessboardView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -45,7 +45,8 @@ import java.util.*
 const val MAX_COUNT_IN_LINE = 5
 const val MAX_LINE = 15
 private var isbattlemode = true
-private var Path = "https://s3.amazonaws.com/appsdeveloperblog/Micky.jpg"
+//private var Path = "https://vignette.wikia.nocookie.net/naruto/images/0/09/Naruto_newshot.png"
+private var Path = ""
 private var model: MyViewModel? = null
 
 
@@ -58,14 +59,18 @@ class MainActivity : AppCompatActivity() {
         val intentFilter = IntentFilter("android.intent.action.AIRPLANE_MODE")
         val factory = Factory()
         model = ViewModelProvider(this, factory).get(MyViewModel::class.java)
+        val urltext: EditText = findViewById(R.id.background_inputurl)
+        urltext.isVisible = false
+        download_button.isVisible = false
+        val back_spinner: Spinner = findViewById(R.id.background_spinner)
+
+
         val receiver: BroadcastReceiver = object : BroadcastReceiver() {
             override fun onReceive(context: Context?, intent: Intent?) {
                 if (intent != null) {
                     if (intent.getBooleanExtra("state", false)) {
                         findViewById<Button>(R.id.download_button).setBackgroundColor(Color.GRAY)
-                    }
-                    else
-                    {
+                    } else {
                         findViewById<Button>(R.id.download_button).setBackgroundColor(Color.LTGRAY)
                     }
                 }
@@ -132,29 +137,36 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        // no longer need it, replace it with spinner implemented below
         // change background button
-        changebackground_button.setOnClickListener {
-            //check runtime permission
-            if (VERSION.SDK_INT >= VERSION_CODES.M) {
-                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
-                    PackageManager.PERMISSION_DENIED
-                ) {
-                    //permission denied
-                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE);
-                    //show popup to request runtime permission
-                    requestPermissions(permissions, PERMISSION_CODE);
-                } else {
-                    //permission already granted
-                    pickImageFromGallery();
-                }
-            } else {
-                //system OS is < Marshmallow
-                pickImageFromGallery();
-            }
-        }
+//        changebackground_button.setOnClickListener {
+//            //check runtime permission
+//            if (VERSION.SDK_INT >= VERSION_CODES.M) {
+//                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
+//                    PackageManager.PERMISSION_DENIED
+//                ) {
+//                    //permission denied
+//                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE);
+//                    //show popup to request runtime permission
+//                    requestPermissions(permissions, PERMISSION_CODE);
+//                } else {
+//                    //permission already granted
+//                    pickImageFromGallery();
+//                }
+//            } else {
+//                //system OS is < Marshmallow
+//                pickImageFromGallery();
+//            }
+//        }
 
 
+        //download button
         download_button.setOnClickListener {
+            Path = urltext.text.toString()
+            urltext.isVisible = false
+            download_button.isVisible = false
+            back_spinner.setSelection(0)
+
             if (ContextCompat.checkSelfPermission(
                     this,
                     Manifest.permission.WRITE_EXTERNAL_STORAGE
@@ -182,13 +194,65 @@ class MainActivity : AppCompatActivity() {
                 }
             } else {
                 // Permission has already been granted
-//                Toast.makeText(this, "in 1st else", Toast.LENGTH_SHORT).show()
                 val externalStorageState = Environment.getExternalStorageState()
                 if (externalStorageState.equals(Environment.MEDIA_MOUNTED)) {
                     DonwloadSaveImg.donwloadImg(this, Path)
+                    pickImageFromGallery();
                 }
             }
         }
+
+
+        /*
+        spinner for changing background
+         */
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.backgroundselection_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            back_spinner.adapter = adapter
+        }
+
+        back_spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selected = parent?.getItemAtPosition(position).toString()
+                if (selected == "select local picture") {
+                    //check runtime permission
+                    if (VERSION.SDK_INT >= VERSION_CODES.M) {
+                        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                            PackageManager.PERMISSION_DENIED
+                        ) {
+                            //permission denied
+                            val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE);
+                            //show popup to request runtime permission
+                            requestPermissions(permissions, PERMISSION_CODE);
+                        } else {
+                            //permission already granted
+                            pickImageFromGallery();
+                        }
+                        back_spinner.setSelection(0)
+                    } else {
+                        //system OS is < Marshmallow
+                        pickImageFromGallery();
+                    }
+                } else if (selected == "use picture online") {
+                    urltext.isVisible = true
+                    download_button.isVisible = true
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                return
+            }
+        }
+
     }
 
     companion object {
@@ -197,18 +261,20 @@ class MainActivity : AppCompatActivity() {
 
         //Permission code
         private val PERMISSION_CODE = 1001;
+
         //upload route
         const val URL = "https://posthere.io/"
+
         //upload route key
-        const val ROUTE ="0ccd-4a3d-9d01"
+        const val ROUTE = "0ccd-4a3d-9d01"
 
         const val TAG = "GoBang_TAG"
 
         const val USERNAME = "GoBang"
+
         //val COMPLETE_INTENT = "complete intent"
         //val MUSICNAME = "music name"
-        fun event(color: String, x: Int, y: Int)
-        {
+        fun event(color: String, x: Int, y: Int) {
             model?.appendEvent(color, x, y)
         }
 
@@ -251,10 +317,6 @@ class MainActivity : AppCompatActivity() {
 //    } 已弃用 对话框
 
 
-
-
-
-
     private fun pickImageFromGallery() {
         //Intent to pick image
         val intent = Intent(Intent.ACTION_PICK)
@@ -268,7 +330,6 @@ class MainActivity : AppCompatActivity() {
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-//        Toast.makeText(this, "requestCode is : " + requestCode, Toast.LENGTH_SHORT).show()
         when (requestCode) {
             1001 -> {
                 // If request is cancelled, the result arrays are empty.
@@ -289,6 +350,8 @@ class MainActivity : AppCompatActivity() {
                     // permission was granted, yay! Do the
                     // contacts-related task you need to do.
                     DonwloadSaveImg.donwloadImg(this, Path)
+                    Thread.sleep(1000L)
+                    pickImageFromGallery();
                 } else {
                     // permission denied, boo! Disable the
                     // functionality that depends on this permission.
@@ -319,14 +382,19 @@ object DonwloadSaveImg {
     private var context: Context? = null
     private var filePath: String? = null
     private var mBitmap: Bitmap? = null
-    private var mSaveMessage = "失败"
+    private var mSaveMessage = "Failed ! ! ! Please input the correct URL"
     private const val TAG = "PictureActivity"
     private var mSaveDialog: ProgressDialog? = null
     fun donwloadImg(contexts: Context?, filePaths: String?) {
         context = contexts
         filePath = filePaths
         mSaveDialog =
-            ProgressDialog.show(context, "保存图片", "图片正在保存中，请稍等...", true)
+            ProgressDialog.show(
+                context,
+                "Save image",
+                "Please wait, downloading image to phone",
+                true
+            )
         Thread(saveFileRunnable).start()
     }
 
@@ -342,9 +410,9 @@ object DonwloadSaveImg {
                 inputStream.close()
             }
             saveFile(mBitmap)
-            mSaveMessage = "图片保存成功！"
+            mSaveMessage = "Image saved ! ! !"
         } catch (e: IOException) {
-            mSaveMessage = "图片保存失败！"
+            mSaveMessage = "Fail to download image ! ! !"
             e.printStackTrace()
         } catch (e: Exception) {
             e.printStackTrace()
@@ -365,7 +433,7 @@ object DonwloadSaveImg {
     }
 
     /**
-     * 保存图片
+     * save image
      * @param bm
      * @throws IOException
      */
@@ -390,11 +458,6 @@ object DonwloadSaveImg {
         intent.data = uri
         context?.sendBroadcast(intent)
     }
-
-
-
-
-
 
 
 }
